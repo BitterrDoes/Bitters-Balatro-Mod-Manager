914cce7631467fe78cd67b11af275173a32003ec
BitterrDoes <bitterdoes420@gmail.com>
2025-11-23 23:06:53 +1100

    I sure hope nothing's broken

--- main.py
+++ main.py
@@ -3,9 +3,9 @@ import sys
 import time
 import json5
 import shutil
-import random
 import requests
 import threading
+import webbrowser # :( yet another import bloating my software
 import subprocess
 from PIL import Image
 from io import BytesIO
@@ -88,13 +88,19 @@ def deleteMod(mod_folder, row):
     row.destroy()
 
 # load all mods
-def load_mods(frame):
+def load_mods(frame, ctkBut, ctkButtons):
     global busy
     if busy: show_admin_warning("Jeez give it a bloody second"); return
     busy = True
 
     for widget in frame.winfo_children():
         widget.destroy()
+
+    if ctkBut: # fix my fuck up
+        for btn in ctkButtons:
+            # Im lazy so you get a for loop
+            ctkButtons[btn].configure(fg_color="#12121A", hover_color="#222235")
+        ctkBut.configure(fg_color="#6366F1", hover_color="#6366F1")
 
     # check for mod path, if no, then make it
     if not os.path.exists(MODS_PATH):
@@ -109,7 +115,16 @@ def load_mods(frame):
     if len(os.listdir(MODS_PATH)) == 0:
         ctk.CTkLabel(frame, text = "You dont have any mods..", font=("Comic Sans MS", 32), pady=8).pack()
         ctk.CTkLabel(frame, text = "try going to #modding in the balatro server", font=("Comic Sans MS", 24), pady=8).pack()
-        discordicon = Image.open(resource_path("bin.png"))
+
+        discordicon = resource_path("Placeholder.png")
+        invitelink = "https://discord.gg/balatro"
+        link = "https://discord.gg/1116389027176787968/1209506514763522108"
+        link = link.replace("https://", "discord://")
+        ctk.CTkButton(frame,
+                      image=ctk.CTkImage(Image.open(discordicon), size=(24, 24)),      
+                      text = "Balatro Discord Server", font=("Comic Sans MS", 16),      
+                      command=lambda: (webbrowser.open_new(invitelink), webbrowser.open_new(link))
+                      ).pack(pady=8)
 
         busy = False
 
@@ -203,9 +218,7 @@ def load_mods(frame):
     versions = []
     seen_versions = set()
 
-    # smodsRow = ctk.CTkFrame(frame, fg_color="#1E1E1E")
-    # smodsRow.place(relx=0.5, rely=0.5, anchor="center")
-    smodsRow = ctk.CTkFrame(frame, fg_color="#111")
+    smodsRow = ctk.CTkFrame(frame, fg_color="#0A0A0F")
     smodsRow.pack(fill="x")
 
     for folder in os.listdir(SMODS_PATH):
@@ -213,12 +226,12 @@ def load_mods(frame):
         if not os.path.isdir(folder_path):
             continue
         
-        version = None
 
         version_path = os.path.join(folder_path, "version.lua")
 
         if os.path.exists(version_path):
             try:
+                version = None
                 with open(version_path, "r", encoding="utf-8") as f:
                     lua_src = f.read()
 
@@ -228,16 +241,16 @@ def load_mods(frame):
                     if isinstance(node, astnodes.Return) and isinstance(node.values[0], astnodes.String): # should always be 0, since it doesnt ever have more than 1 return, istg if john smods changes it
                         version = node.values[0].s
                         break
+            
+                if version and version not in seen_versions:
+                    seen_versions.add(version)
+                    versions.append({
+                        "folder_path": folder_path,
+                        "folder_name": folder,
+                        "version": version,
+                    })
             except:
                 pass
-
-        if version and version not in seen_versions:
-            seen_versions.add(version)
-            versions.append({
-                "folder_path": folder_path,
-                "folder_name": folder,
-                "version": version,
-            })
 
     def changeSmods(smod): # smod = [folder_path: string, version: string, folder_name: string]
         smod_path = smod["folder_path"]
@@ -250,7 +263,7 @@ def load_mods(frame):
         shutil.copytree(smod_path, os.path.join(MODS_PATH, smod_name))
 
         # Relllooooadd
-        load_mods(frame)
+        load_mods(frame, None, None)
 
     found = False
     def addsmod(smod):
@@ -259,10 +272,10 @@ def load_mods(frame):
 
         selected = False 
 
-        color = "#1E1E1E"
+        color = "#12121A"
         # Check if current smods == this one
         if smod_ver == Currentsmods["version"]:
-            color = "#2A2A2A"
+            color = "#181822"
             selected = True 
             found = smod_path
 
@@ -272,7 +285,7 @@ def load_mods(frame):
         smod_ver = smod_ver[0:smod_ver.find("-")] # mitosis
 
         # make button
-        btn = ctk.CTkButton(smodsRow, fg_color=color, hover_color = "#3B3B3B", font=("Comic Sans MS", 24), text=smod_ver, command=lambda s=smod: changeSmods(s))
+        btn = ctk.CTkButton(smodsRow, fg_color=color, hover_color = "#222235", font=("Comic Sans MS", 24), text=smod_ver, command=lambda s=smod: changeSmods(s))
         btn.pack(expand=True,side="left",pady=8)
 
     for smod in versions:
@@ -303,8 +316,8 @@ def load_mods(frame):
 
         name = data.get("display_name") or data.get("name")
         author = ", ".join(data.get("author", [])) if data.get("author") else ""
-        color = "#1E1E1E" if index == 1 else "#2A2A2A"
-        hover_color = "#3B3B3B"
+        color = "#12121A" if index == 1 else "#181822"
+        hover_color = "#222235"
         row = ctk.CTkFrame(frame, fg_color=color)
         row.pack(fill="x", padx=10, pady=10)
 
@@ -328,7 +341,7 @@ def load_mods(frame):
         image_label.pack(side="left", padx=10)
 
         # title
-        name_label = ctk.CTkLabel(row, text=name, text_color="#fff", font=("Comic Sans MS", 16))
+        name_label = ctk.CTkLabel(row, text=name, font=("Comic Sans MS", 16))
         name_label.pack(side="left", padx=10)
 
         # author
@@ -357,18 +370,24 @@ def load_mods(frame):
 
         index += 1
         
-        # add funny nothing button
-        nothn_btn = ctk.CTkButton(frame, text="Button that does nothing", font=("Comic Sans MS", 24, "normal", "italic"))
-        nothn_btn.pack(padx=5)
-        busy = False
+    # add funny nothing button
+    nothn_btn = ctk.CTkButton(frame, text="Button that does nothing", font=("Comic Sans MS", 24, "normal", "italic"))
+    nothn_btn.pack(padx=5)
+    busy = False
 
 # the info funcs
-def load_download(frame):
+def load_download(frame, ctkBut, ctkButtons):
     global busy
     if busy: show_admin_warning("Jeez give it a bloody second"); return
     busy = True
     for w in frame.winfo_children():
         w.destroy()
+
+    if ctkBut: # fix my fuck up
+        for btn in ctkButtons:
+            # Im lazy so you get a for loop
+            ctkButtons[btn].configure(fg_color="#12121A", hover_color="#222235")
+        ctkBut.configure(fg_color="#6366F1", hover_color="#6366F1")
 
     warnlabel = ctk.CTkLabel(frame, text="uhhh this might take a sec...")
     warnlabel.pack()
@@ -459,7 +478,7 @@ def load_download(frame):
 
             while len(threads) >= 5:
                 time.sleep(0.1)
-                threads = [t for t in threads if t.is_alive()]  
+                threads = [t for t in threads if t.is_alive()]      
 
             curthread = threading.Thread(target=lambda: loadmod_cool(f))            
             threads.append(curthread)
@@ -479,136 +498,101 @@ def load_download(frame):
 def createInfobuttons(frame, modframe): # frame = sidemenu, modframe = the frame for mods
     # the set menu buttons
     button_configs = [ # thanks to the guy from discord
-        {"pady": 7.5, "func": load_mods, "title": "Installed Mods"},
-        {"pady": 0, "func": load_download, "title": "Download Mods"},
+        {"pady": 7.5, "func": load_mods, "title": "Installed"},
+        {"pady": 0, "func": load_download, "title": "Browse"},
         {"pady": 7.5, "func": load_download, "title": "Modpacks"},
     ]
+    buttons = {}
     for config in button_configs:
-        ctkbut = ctk.CTkButton(frame, text=config["title"], font=("Comic Sans MS", 24), fg_color="#2A2A2A", hover_color="#242424", command=lambda c = config["func"],: c(modframe))
-        ctkbut.pack(fill="x", padx=15, pady=config["pady"])
+        ctkbut = ctk.CTkButton(frame, text=config["title"], font=("Comic Sans MS", 24), fg_color="#12121A", hover_color="#222235")
+        ctkbut.configure(command=lambda c = config["func"], b = ctkbut: c(modframe, b, buttons))
+        if button_configs[0] == config:
+            ctkbut.configure(fg_color="#6366F1", hover_color="#6366F1")
+        ctkbut.pack(fill="x", padx=15, pady=config["pady"], ipady = 3)
+        buttons[len(buttons)] = ctkbut
+        print(ctkbut, buttons)
 
 # App button Funcs
 def EnableMods():
     for mod in mod_list.winfo_children():
-        if not mod.enabled:
-            toggle_mod(mod.folder_path, mod.toggle_btn)
+        try:
+            if not mod.enabled:
+                toggle_mod(mod.folder_path, mod.toggle_btn)
+        except:
+            pass
 
 def DisableMods():
     for mod in mod_list.winfo_children():
-        if mod.enabled:
-            toggle_mod(mod.folder_path, mod.toggle_btn)
+        try:
+            if mod.enabled:
+                toggle_mod(mod.folder_path, mod.toggle_btn)
+        except:
+            pass
 
 def OpenFolder():
     subprocess.Popen('explorer ' + MODS_PATH)
-
-Mods = [
-    "Angled Bitterness",
-    "Milky's Bullshit",
-    "Busted Buffoons",
-    "Balala",
-    "Terraria",
-    "Aikoyori's shenanigans",
-    "Tangents",
-    "Lucky Rabbit",
-    "Hot potato",
-    "Cold Beans",
-    "Garbshit",
-    "Jambatro",
-    "Vall-karri",
-    "Maniatro",
-    "Balatropit! actually probably dont.",
-    "Ultrakill"
-]
-
-Music = [
-    "Ween",
-    "Primus",
-    "MiLKY-P",
-    "Slipknot",
-    "Arcanaut",
-    "femtanyl",
-    "Nick Drake",
-    "Mr. Bungle",
-    "Radio head",
-    "Jamiroquai",
-    "Sound Garden",
-    "Eliott Smith",
-    "Stevie Wonder",
-    "Geordie Greep",
-    "System Of A Down",
-    "Tyler the Creator",
-    "Queens of the Stone Age",
-]
-
-Titles = [
-    ":3",
-    "Play " + random.choice(Mods),
-    "Go listen to " + random.choice(Music)
-]
-
-msgs = [
-    "say gex",
-    "go play " + random.choice(Mods),
-    "go listen to " + random.choice(Music),
-    "Die",
-    ":3",
-    "WARNING! popup may explode",
-    "please dont make more popups",
-    "wawa"
-]
-
-def popup(e):
-    if e.num == 3:
-        show_admin_warning(random.choice(msgs))
 # --- APP SETUP ---
 ctk.set_appearance_mode("dark")
-ctk.set_default_color_theme("dark-blue")
+ctk.set_default_color_theme(resource_path("theme.json"))
 
 
 app = ctk.CTk()
-app.title("Bitter's bmm | " + random.choice(Titles))
+app.title("Bitter's bmm")
 app.geometry("1200x800")
 
 # top frame
-top_frame = ctk.CTkFrame(app, fg_color="#111")
+top_frame = ctk.CTkFrame(app, height=120)
 top_frame.pack(fill="x")
 
-title = ctk.CTkButton(top_frame, text="Bitter's mod manager", fg_color="transparent", font=("Comic Sans MS", 20, "bold"), command=lambda: load_mods(mod_list))
-title.pack(side="left", padx=15, pady=10)
-title.bind("<Button>", popup)
+# Container
+Title = ctk.CTkFrame(top_frame, fg_color="transparent")
+Title.pack(side="left", padx=16)
+# Kitty
+ctk.CTkLabel(Title, text="", image=ctk.CTkImage( Image.open( resource_path( "Kittty8Bit.png" ) ), size=(24, 24) ) ).pack(side="left", padx=3)
+# Bitter (THSTEFJNDC ME!!!)
+ctk.CTkLabel(Title, text="Bitter's", font=("Comic Sans MS", 24, "bold")).pack(side="left", padx=4)
+# Superscript
+ctk.CTkLabel(Title, text="Mod Manager", font=("Comic Sans MS", 16, "italic")).pack(padx=5)
 
-start_btn = ctk.CTkButton(top_frame, text="Start Game", command=start_game, font=("Comic Sans MS", 16))
-start_btn.pack(side="right", padx=15, pady=10)
+# start
+start_btn = ctk.CTkButton(top_frame, text="▶ Start Game", command=start_game, font=("Comic Sans MS", 16))
+start_btn.pack(side="right", padx=15, pady=10, ipady=5)
 
-# toggle buttons
+# Center buttons
 btn_frame = ctk.CTkFrame(top_frame, fg_color="transparent") # so like its placed right
 btn_frame.place(relx=0.5, rely=0.5, anchor="center")
 
+# Refresh
+RefreshIcon = resource_path("Folder.png")  # finally I can check my mods folder
+Refresh_btn = ctk.CTkButton(btn_frame, image=ctk.CTkImage(Image.open(RefreshIcon), size=(20,20)), text="Refresh", command=lambda: load_mods(mod_list, None, None), font=("Comic Sans MS", 16), fg_color="#1B3B44", hover_color="#0C2228")
+Refresh_btn.grid(row=0, column=0, padx=10, ipady = 5)
+
+# Disable
 disablebtn = ctk.CTkButton(btn_frame, text="Disable Mods", command=DisableMods, font=("Comic Sans MS", 16), fg_color="#E53552", hover_color="#9E1B31")
-disablebtn.grid(row=0, column=0, padx=10)
+disablebtn.grid(row=0, column=1, padx=10, ipady = 5)
 
+# Enable
 enablebtn = ctk.CTkButton(btn_frame, text="Enable Mods", command=EnableMods, font=("Comic Sans MS", 16), fg_color="#4CAF81", hover_color="#1F8A58")
-enablebtn.grid(row=0, column=1, padx=10)
+enablebtn.grid(row=0, column=2, padx=10, ipady = 5)
 
-# Mod Folder Button
+# Folder
 folderIcon = resource_path("Folder.png")  # finally I can check my mods folder
-folderImg = ctk.CTkImage(dark_image=Image.open(folderIcon))
-folder_btn = ctk.CTkButton(top_frame, image=folderImg, text="", command=OpenFolder, width=24, fg_color="#A3931F", hover_color="#82740B") # add command=lambda: If variables to be added
-folder_btn.pack(side="left", padx=3, pady=10)
+folder_btn = ctk.CTkButton(btn_frame, image=ctk.CTkImage(Image.open(folderIcon), size=(20,20)), text="Downloaded", command=OpenFolder, font=("Comic Sans MS", 16), fg_color="#1B3B44", hover_color="#0C2228")
+folder_btn.grid(row=0, column=3, padx=10, ipady = 5)
 
 # mod list frame
-frame_outer = ctk.CTkFrame(app, fg_color="#151515")
+frame_outer = ctk.CTkFrame(app, fg_color="#0A0A0F")
 frame_outer.pack(fill="both", expand=True, padx=3, pady=10)
 
-mod_list = ctk.CTkScrollableFrame(frame_outer)
+mod_list = ctk.CTkScrollableFrame(frame_outer, fg_color="#12121A")
 mod_list.pack(side="right", fill="both", ipadx = 325, expand=True, padx=10, pady=10)
 
-Infoframe = ctk.CTkFrame(frame_outer)
+Infoframe = ctk.CTkFrame(frame_outer, fg_color="#12121A")
 Infoframe.pack(side="left", fill="both", ipadx = 33, expand=True, padx=10, pady=10)
 createInfobuttons(Infoframe, mod_list)
 
 
-load_mods(mod_list)
+load_mods(mod_list, None, None)
 
 app.grid_columnconfigure(0, weight=1)
 app.mainloop()
